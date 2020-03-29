ebc24937fd6ec56b52955e0afe4e64e1e1c56b6b
piotr rawski <rawski.piotr@yahoo.pl>
2020-03-29 10:07:01 +0000

    Dodano możliwość zmiany statusu zadania

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,9 +9,4 @@ router.post('/add', async (ctx, next) => {
   ctx.redirect('/')
 })
 
-router.post('/updateStatus', async (ctx, next) => {
-  const { id, status } = ctx.request.body
-  await store.updateTaskStatus(id, status)
-  ctx.status = 200
-})
 module.exports = router
